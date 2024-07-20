9e7dd772428bc3e9d2810ebdd5fb07a89c188136
KenTubio <tubiokenneth40@gmail.com>
2024-07-20 23:49:54 +0800

    updated the add to cart

--- dist/js/ecomms-continuation.js
+++ dist/js/ecomms-continuation.js
@@ -7,18 +7,82 @@ const maxScroll = container.scrollWidth - container.offsetWidth;
 
 nextBtn.addEventListener("click", () => {
   container.style.scrollBehavior = "smooth";
-  if(container.scrollLeft >= maxScroll){
+  if (container.scrollLeft >= maxScroll) {
     container.scrollLeft = 0;
-  }else {
+  } else {
     container.scrollLeft += container.offsetWidth;
   }
 });
 
 beforeBtn.addEventListener("click", () => {
-    container.style.scrollBehavior = "smooth";
-    if(container.scrollLeft >= maxScroll){
-      container.scrollLeft = 0;
-    }else {
-      container.scrollLeft -= container.offsetWidth;
-    }
-  });
+  container.style.scrollBehavior = "smooth";
+  if (container.scrollLeft >= maxScroll) {
+    container.scrollLeft = 0;
+  } else {
+    container.scrollLeft -= container.offsetWidth;
+  }
+});
+
+//POPUP ADD TO CART FUNCTIONS TO DITO
+const closeBtn = document.getElementById("close");
+const popupContainer = document.getElementById("popup-container");
+const popup = document.getElementById("popup");
+const cart = document.getElementById("cart");
+const popupContent = document.getElementById("popup-content");
+
+
+cart.addEventListener("click", () => {
+  popupContainer.classList.toggle("hidden");
+  document.body.style.overflowY = 'hidden';
+});
+
+closeBtn.addEventListener("click", () => {
+  popupContainer.classList.add("hidden");
+});
+
+popupContainer.addEventListener("click", () => {
+  popupContainer.classList.add("hidden");
+  document.body.style.overflowY = 'scroll';
+});
+
+
+//ADD TO CART ITEMS TO DITO BANDA
+
+document.querySelectorAll('#add-to-cart').forEach(button => {
+  button.addEventListener('click', ()=>{
+
+    const itemContainer = document.createElement('div');
+    itemContainer.classList.add('border-2');
+    popupContent.appendChild(itemContainer);
+
+    const imgDisplay = button.getAttribute('data-image-src');
+    const itemImageContent = document.createElement('img');
+    itemImageContent.src = imgDisplay;
+    itemImageContent.classList.add('w-52');
+    itemImageContent.classList.add('h-52');
+
+    button.style.pointerEvents = 'none';
+    button.style.opacity = '.3';
+
+    itemContainer.appendChild(itemImageContent);
+
+  })
+});
+
+
+//DITO YUNG MAG DISPLAY ANG NUMBER SA CART
+const cartNumDisplay = document.getElementById('cart-numdisplay');
+
+let countDisplay = parseInt(cartNumDisplay.textContent, 10);
+
+function countPerClick() {
+  countDisplay ++;
+
+  cartNumDisplay.textContent = countDisplay;
+}
+
+const allCartBtn = document.querySelectorAll('#add-to-cart');
+
+document.querySelectorAll('#add-to-cart').forEach(button =>{
+  button.addEventListener('click', countPerClick);
+})
